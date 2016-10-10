f50ee7d0df3dfec0c58f23a3c2ca675566641f96
astrohoff <astrohoff@ufl.edu>
2016-10-10 07:15:36 -0400

    Started creating a radial triangle based approach to mesh generation.

---
+++ Assets/Scripts/FunctionController.cs
@@ -0,0 +1,107 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using org.mariuszgromada.math.mxparser;
+
+public class FunctionController : MonoBehaviour {
+    public string functionString = "f(x, y)=x^2+y^2";
+    float drawBound = 10;
+    int boundDivisions = 24;
+    public Transform player;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    Mesh GetFunctionMesh(Function function, float segmentLength)
+    {
+        if(function.checkSyntax() == false)
+        {
+            return GetErrorMesh(player.localScale.x);
+        }
+        Mesh fMesh = new Mesh();
+        List<Vector3> vertices = new List<Vector3>();
+        List<int> indices = new List<int>();
+        if(function.getArgumentsNumber() == 1)
+        {
+
+        }
+        else if(function.getArgumentsNumber() == 2)
+        {
+            int vertCount;
+            float angleStep;
+            float theta;
+            float radius;
+            float x = 0;
+            float z = 0;
+            float y = (float)function.calculate(x, z);
+            vertices.Add(new Vector3(x, y, z));
+            for (int ring = 1; ring * segmentLength <= drawBound; ring++)
+            {
+                vertCount = Mathf.RoundToInt(2 * Mathf.PI * ring);
+                angleStep = 360 / vertCount;
+                radius = ring * segmentLength;
+                for (int vert = 0; vert < vertCount; vert++)
+                {
+                    theta = angleStep * vert;
+                    x = Mathf.Cos(theta) * radius;
+                    z = Mathf.Sin(theta) * radius;
+                    y = (float)function.calculate(x, z);
+                    vertices.Add(new Vector3(x, y, z));
+                }
+            }
+        }
+        return fMesh;
+    }
+
+    List<int> GetIndices(int numRings)
+    {
+        List<int> indices = new List<int>();
+        indices.Add(0);
+        for(int ring = 1; ring < numRings - 1; ring++)
+        {
+            /*float outTrisPerVert = (float)ringVertCount / prevRingVertCount;
+            float outTris = 0;
+            for(int vert = 0; vert < ringVertCount; vert++)
+            {
+                outTris += outTrisPerVert;
+
+            }*/
+            int ringVertCount = Mathf.RoundToInt(2 * Mathf.PI * ring);
+            int ringVertOffset = GetVertexOffset(ring);
+            int nextRingVertCount = Mathf.RoundToInt(2 * Mathf.PI * (ring + 1));
+        }
+
+    }
+
+    int GetVertexOffset(int ringNum)
+    {
+        if(ringNum == 0)
+        {
+            return 0;
+        }
+        int offset = 1;
+        for(int ring = 1; ring <= ringNum; ring++)
+        {
+            offset += Mathf.RoundToInt(2 * Mathf.PI * (ring - 1));
+        }
+        return offset;
+    }
+
+    // Return "X" error mesh.
+    Mesh GetErrorMesh(float scale)
+    {
+        Mesh eMesh = new Mesh();
+        Vector3[] vertices = new Vector3[4] {new Vector3(-scale, -scale, 0), new Vector3(scale, scale, 0),
+                                             new Vector3(-scale, scale, 0), new Vector3(scale, -scale, 0)};
+        int[] indices = new int[4] { 0, 1, 2, 3 };
+        eMesh.vertices = vertices;
+        eMesh.SetIndices(indices, MeshTopology.Lines, 0);
+        return eMesh;
+    }
+}
---
+++ Assets/Scripts/GridController.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GridController : MonoBehaviour {
+    public float renderDistance = 100;
+    public float minorUnits = 1;
+    public float majorUnits = 4;
+    Mesh gridMesh;
+
+	void Start () {
+        gridMesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = gridMesh;
+        BuildGrid();
+	}
+
+    void BuildGrid()
+    {
+        List<Vector3> vertices = new List<Vector3>();
+        List<int> majorIndices = new List<int>();
+        List<int> minorIndices = new List<int>();
+        // Generate vertices and indices for grid mesh.
+        for(float z = -renderDistance; z <= renderDistance; z += minorUnits)
+        {
+            for (float y = -renderDistance; y <= renderDistance; y += minorUnits)
+            {
+                for (float x = -renderDistance; x <= renderDistance; x += minorUnits)
+                {
+                    if(x == -renderDistance)
+                    {
+                        if(Mathf.Abs(y % majorUnits) < minorUnits / 10 && Mathf.Abs(z % majorUnits) < minorUnits / 10)
+                        {
+                            AddXLine(y, z, ref vertices, ref majorIndices);
+                        }
+                        else
+                        {
+                            AddXLine(y, z, ref vertices, ref minorIndices);
+                        }
+                    }
+                    if(y == -renderDistance)
+                    {
+                        if(Mathf.Abs(x % majorUnits) < minorUnits / 10 && Mathf.Abs(z % majorUnits) < minorUnits / 10)
+                        {
+                            AddYLine(x, z, ref vertices, ref majorIndices);
+                        }
+                        else
+                        {
+                            AddYLine(x, z, ref vertices, ref minorIndices);
+                        }
+                    }
+                    if(z == -renderDistance)
+                    {
+                        if(Mathf.Abs(x % majorUnits) < minorUnits / 10 && Mathf.Abs(y % majorUnits) < minorUnits / 10)
+                        {
+                            AddZLine(x, y, ref vertices, ref majorIndices);
+                        }
+                        else
+                        {
+                            AddZLine(x, y, ref vertices, ref minorIndices);
+                        }
+                    }
+                }
+            }
+        }
+        gridMesh.SetVertices(vertices);
+        gridMesh.subMeshCount = 2;
+        gridMesh.SetIndices(majorIndices.ToArray(), MeshTopology.Lines, 0);
+        gridMesh.SetIndices(minorIndices.ToArray(), MeshTopology.Lines, 1);
+        gridMesh.RecalculateBounds();
+    }
+    void AddXLine(float y, float z, ref List<Vector3> vertices, ref List<int> indices)
+    {
+        indices.Add(vertices.Count);
+        vertices.Add(new Vector3(-renderDistance, y, z));
+        indices.Add(vertices.Count);
+        vertices.Add(new Vector3(renderDistance, y, z));
+    }
+    void AddYLine(float x, float z, ref List<Vector3> vertices, ref List<int> indices)
+    {
+        indices.Add(vertices.Count);
+        vertices.Add(new Vector3(x, -renderDistance, z));
+        indices.Add(vertices.Count);
+        vertices.Add(new Vector3(x, renderDistance, z));
+    }
+    void AddZLine(float x, float y, ref List<Vector3> vertices, ref List<int> indices)
+    {
+        indices.Add(vertices.Count);
+        vertices.Add(new Vector3(x, y, -renderDistance));
+        indices.Add(vertices.Count);
+        vertices.Add(new Vector3(x, y, renderDistance));
+    }
+}
